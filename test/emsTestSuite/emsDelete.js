2201dc8bae798c47c5539bc49b491aaeed7f8076
Madhavi <madhavikh@cybage.com>
2015-08-27 14:23:10 +0530

    1st commit

---
+++ test/emsTestSuite/emsDelete.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'Delete Test for EMS-APPLICATION' : function (client) {
+	  client
+      .url('http://localhost:3000/#/getData')
+      .waitForElementVisible('body', 1000)
+      .assert.title('Employee Application')
+      .click('body > div.ng-scope > div > div > table > thead > tr:nth-child(2) > td:nth-child(5) > input')
+	  .url('http://localhost:3000/#/getData')
+	  .waitForElementVisible('body', 1000)
+	  .getText('body > div.ng-scope > div > div > table > thead > tr:nth-child(2) > td:nth-child(1)',function(result){
+		  this.assert.equal(result.value,'body > div.ng-scope > div > div > table > thead > tr:nth-child(2) > td:nth-child(1)');
+		  
+	  })
+	  .end();
+     
+  },
+
+}
